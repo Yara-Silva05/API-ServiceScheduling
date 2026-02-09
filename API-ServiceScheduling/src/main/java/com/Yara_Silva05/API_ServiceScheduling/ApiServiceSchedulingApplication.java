94fb809472702066333ebba11feb30f6ba30a457
Yara-Silva05 <yara.silva.alves05@gmail.com>
2026-02-09 12:51:57 -0300

    Primeiro commit

---
+++ API-ServiceScheduling/src/main/java/com/Yara_Silva05/API_ServiceScheduling/ApiServiceSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.Yara_Silva05.API_ServiceScheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServiceSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServiceSchedulingApplication.class, args);
+	}
+
+}
---
+++ API-ServiceScheduling/src/test/java/com/Yara_Silva05/API_ServiceScheduling/ApiServiceSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yara_Silva05.API_ServiceScheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
