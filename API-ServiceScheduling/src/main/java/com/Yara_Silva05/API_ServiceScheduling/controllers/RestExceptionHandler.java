1f61a486abc32cb44d88b9015f90a55720cb7aa8
Yara-Silva05 <yara.silva.alves05@gmail.com>
2026-02-10 01:04:32 -0300

    Feat: Criação do RestExceptionHandler.

---
+++ API-ServiceScheduling/src/main/java/com/Yara_Silva05/API_ServiceScheduling/controllers/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.Yara_Silva05.API_ServiceScheduling.controllers;
+
+import com.Yara_Silva05.API_ServiceScheduling.exceptions.EmailExistenteException;
+import com.Yara_Silva05.API_ServiceScheduling.exceptions.UsuarioNaoEncontradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    private ResponseEntity<String> usuarioNaoEncontradoHandler(UsuarioNaoEncontradoException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(EmailExistenteException.class)
+    private ResponseEntity<String> emailExistenteHandler(EmailExistenteException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<String> excecaoGenericaHandler(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + exception.getMessage());
+    }
+}
